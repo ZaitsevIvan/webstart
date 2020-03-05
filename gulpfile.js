23b12b5e10dbb00e7f611143da90e35c1b29fe5f
Пользователь <rasstafarik@gmail.com>
2020-03-05 12:47:35 +0300

    feature: add  gulp-sass in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,39 @@
-const gulp = require('gulp');
-const browserSync = require('browser-sync').create();
-const rename = require('gulp-rename');
-const cleanCSS = require('gulp-clean-css');
-
+const {src, dest, watch}  = require('gulp');
+const browserSync     = require('browser-sync').create();
+const rename          = require('gulp-rename');
+const cleanCSS        = require('gulp-clean-css');
+const sass            = require('gulp-sass');
 
 // Static server
-gulp.task('browser-sync', function() {
+function bs() {
+  serveSass;
   browserSync.init({
       server: {
           baseDir: "./"
       }
   });
-  gulp.watch("./*.html").on('change', browserSync.reload);
-});
+  watch("./*.html").on('change', browserSync.reload);
+  watch("./sass/**/*.sass", serveSass);
+  watch("*.js").on('change', browserSync.reload);
+};
 
-///minify css + rename
-gulp.task('minify-css', () => {
+//minify css + rename
+function minify() {
   return gulp.src('css/*.css')
   .pipe(cleanCSS({debug: true}, (details) => {
   console.log(`${details.name}: ${details.stats.originalSize}`);
   console.log(`${details.name}: ${details.stats.minifiedSize}`);
   }))
   .pipe(rename({ extname: '.min.css' }))
-  .pipe(gulp.dest('./'));
-  });
+  .pipe(dest('./'));
+  };
+
+  // Compile sass into CSS & auto-inject into browsers
+function serveSass() {
+  return src("./sass/*.sass")
+      .pipe(sass())
+      .pipe(dest("./css"))
+      .pipe(browserSync.stream());
+};
+
+exports.serve = bs;
