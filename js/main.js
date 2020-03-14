dc4ba0fea5e61fd4a95335da855109558bc67bc2
Пользователь <rasstafarik@gmail.com>
2020-03-14 18:42:20 +0300

    feature: add smooth scrollup. refact: rewrite form appearance & hide on jquery

--- js/main.js
+++ js/main.js
@@ -1,29 +1,48 @@
-document.addEventListener("DOMContentLoaded", function(event) { 
+// document.addEventListener("DOMContentLoaded", function(event) { 
 
-  const modal = document.querySelector('.modal');
-  const modalDialog = document.querySelector('.modal__dialog');
-  const modalBtn = document.querySelectorAll('[data-toggle=modal]');
-  const closeBtn = document.querySelector('.modal__close');
+//   const modal = document.querySelector('.modal');
+//   const modalDialog = document.querySelector('.modal__dialog');
+//   const modalBtn = document.querySelectorAll('[data-toggle=modal]');
+//   const closeBtn = document.querySelector('.modal__close');
 
-  const switchModal = () => {
-    modal.classList.toggle('modal--visible');
-  };
+//   document.addEventListener('keyup', function (e) {
+//     if(e.keyCode === 27)
+//      modal.classList.remove('modal--visible');
+//   }); 
 
-  modalBtn.forEach(element => {
-    element.addEventListener('click', switchModal);
+//   modal.addEventListener('click', function(event) {
+//     if (!modalDialog.contains(event.target))
+//     modal.classList.remove('modal--visible');
+//   }); 
+
+
+// });
+
+$(document).ready(function() {
+
+  var modal = $('.modal'),
+      modalBtn = $('[data-toggle="modal"]'),
+      closeBtn = $('.modal__close'),
+      modalDialog = $('.modal__dialog');
+
+      modalBtn.on('click', function () {
+        modal.toggleClass('modal--visible');
+      });
+      closeBtn.on('click', function (){
+        modal.toggleClass('modal--visible');
+      });
+
+$(function() {
+  $(window).scroll(function() {
+    if($(this).scrollTop() != 0) {
+      $('#toTop').fadeIn();
+    } else {
+      $('#toTop').fadeOut();
+    }
   });
 
-  closeBtn.addEventListener('click', switchModal);
-
-  document.addEventListener('keyup', function (e) {
-    if(e.keyCode === 27)
-     modal.classList.remove('modal--visible');
-  }); 
-
-  modal.addEventListener('click', function(event) {
-    if (!modalDialog.contains(event.target))
-    modal.classList.remove('modal--visible');
-  }); 
-
-
+  $('#toTop').click(function() {
+    $('body,html').animate({scrollTop:0},800);
+    });
+  });
 });
