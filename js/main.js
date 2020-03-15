22638851da3db5c9cb8b59db6b21bf648f122220
Пользователь <rasstafarik@gmail.com>
2020-03-15 03:46:16 +0300

    refact: rewrite form appereance on jquery

--- js/main.js
+++ js/main.js
@@ -33,16 +33,32 @@ $(document).ready(function() {
       });
 
 $(function() {
-  $(window).scroll(function() {
-    if($(this).scrollTop() != 0) {
-      $('#toTop').fadeIn();
-    } else {
-      $('#toTop').fadeOut();
+  $(window).scroll(function() {      //событие прокрутки окна
+    if($(this).scrollTop() != 0) {      //если не крайнее верхнее положение
+      $(".topbtn").fadeIn();         //появится кнопка прокрутки наверх
+    } else {                         //иначе 
+      $(".topbtn").fadeOut();         //пропадёт
     }
   });
 
-  $('#toTop').click(function() {
-    $('body,html').animate({scrollTop:0},800);
+  $(".topbtn").click(function() {      //событие клика по кнопке
+    $('body,html').animate({scrollTop:0},800);   //анимированная прокрутка со
+                                                 // скоростью 800
     });
   });
+
+  $(modal).mouseup(function (e){ // событие клика по веб-документу
+		var div = $(".modal__dialog"); // тут указываем  элемент
+		if (!div.is(e.target) // если клик был не по нашему блоку
+		    && div.has(e.target).length === 0) { // и не по его дочерним элементам
+			$(".modal").removeClass("modal--visible") // убираем модификатор у .modal
+		}
+	});
+
+  $(document).keyup(function (e){ // событие нажатия кнопки
+		if (e.which == 27)  { // нажатие на esc
+			$(".modal").removeClass("modal--visible") // убираем модификатор у .modal
+		}
+  });
+  
 });
